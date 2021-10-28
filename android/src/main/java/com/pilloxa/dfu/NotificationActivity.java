ee362437c286f3db89906fef89d33f03ba98fa80
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2021-10-28 10:41:01 +0200

    Revive unpushed changes out of NPM
    Seems like the version published to NPM is at 3.2.0 and this repo is
    only at 3.1.0.  Sounds like some changes were published to NPM without
    pushing to GitHub.
    
    I've downloaded the contents of the 3.2.0 release from NPM and these are
    the changes.

--- android/src/main/java/com/pilloxa/dfu/NotificationActivity.java
+++ android/src/main/java/com/pilloxa/dfu/NotificationActivity.java
@@ -3,7 +3,7 @@ package com.pilloxa.dfu;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.bridge.ReactContext;
